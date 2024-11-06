2846a8c9620346a3dcee8f8c86bb093065a87a33
MUKESH KUMAR <104624633+MUKESH12343@users.noreply.github.com>
2024-11-06 12:09:31 +0530

    Add files via upload

--- Js.js
+++ Js.js
@@ -1,1 +1,10 @@
 console.log("Hello Mukesh ");
+const  accountId = 1443
+let accountEmail = "iammukesh13@gmail.com"
+var accountPassword = "123"
+accountCity = "Chandigarh"
+
+/*
+Perfer not use var 
+Beacause of issue in block of scope and functional scope*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
