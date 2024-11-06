a3423e60423262a646e1842d92dd961406b199b5
MUKESH12343 <iammukesh63@gmail.com>
2024-11-06 12:28:47 +0530

    first timm

---
+++ Js/chapter_1.js
@@ -0,0 +1,21 @@
+// Question 1
+let a = "Mukesh"
+let b = 7
+console.log(a+b)
+
+console.log(typeof (a+b))
+
+const student = {
+        name: "Mukesh",
+        age: 22,
+        class: "BCA",
+        rollNumber: 12345,
+        city: "Bareilly",
+        state: "Uttar Pardesh"
+
+}
+console.log(student)
+
+student  ['city'] = "jambmu"
+ student ['name'] = "kumar"
+ console.log(student)
